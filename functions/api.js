eb1a626acb7753c78e72eb6e7f888f7a2589c5d3
BiometricGlitch <smellysocks0147@gmail.com>
2025-04-07 00:26:23 +0800

    Maybe fix??

--- functions/api.js
+++ functions/api.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const cors = require('cors');
 const path = require('path');
 
 const app = express();
-app.use(cors());
 
 // Set view engine
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 // Route
 app.get('/', (req, res) => {

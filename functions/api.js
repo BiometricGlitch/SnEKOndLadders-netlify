957653c90479b516cfb9a7ed541a3a820495f2e3
BiometricGlitch <smellysocks0147@gmail.com>
2025-04-07 00:16:01 +0800

    Maybe fix??

--- functions/api.js
+++ functions/api.js
@@ -10,10 +10,10 @@ app.engine('pug', require('pug').__express);
 
 // Set view engine
 app.set('view engine', 'pug');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route
 app.get('/', (req, res) => {

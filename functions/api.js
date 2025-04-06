7fd06f8ead2e098f027b65480b668e8d4e04ca73
BiometricGlitch <smellysocks0147@gmail.com>
2025-04-06 21:16:30 +0800

    Initial

---
+++ functions/api.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+
+// Set view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+module.exports.handler = serverless(app);
